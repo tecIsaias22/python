c1af4bd188d0cac9f050a9311d214b84e244f038
tecIsaias22 <tecnologiaisaias22@gmail.com>
2025-04-21 20:42:29 -0300

    Delete Print_input.py

--- Print_input.py
+++
@@ -1,9 +0,0 @@
-nome = input("informe o seu nome:")
-idade = input("informe a sua idade:")
-
-print("nome: " + nome) 
-print("idade: " + idade)
-
-print(nome, idade)  
-print(nome, idade, sep="... \n")
-print(nome, idade, end="#")
